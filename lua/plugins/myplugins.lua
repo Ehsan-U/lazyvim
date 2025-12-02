3f52dc22d8cb2e7e15d3428f00adbd445febf9d1
Ehsan U. <lancerf562@gmail.com>
2025-12-02 16:23:54 +0500

    pyright type checking disabled

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -1,9 +1,21 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = function()
+    opts = function(_, opts)
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<c-k>", false, mode = "i" }
+
+      opts.servers = opts.servers or {}
+      opts.servers.pyright = opts.servers.pyright or {}
+      opts.servers.pyright.settings = {
+        python = {
+          analysis = {
+            typeCheckingMode = "off",
+          },
+        },
+      }
+
+      return opts
     end,
   },
 }
